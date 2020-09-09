cc333fe337c95eb86a92558e1aae21d903859553
elianrc <relian41@yahoo.com>
2020-09-09 15:32:52 -0700

    feat: configs

--- DOCKER_TEST/src/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// You can run a single php script with:
-// sudo docker run -it --rm --name my-running-script -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:7.4-cli php index.php
-
-echo "THIS IS PHP <br> within a DOCKERFILE";
-
-?>
